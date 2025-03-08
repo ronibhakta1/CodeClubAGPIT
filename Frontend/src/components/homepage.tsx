814870888a1b4bf0f4da09a9e6129a94cf1443b2
Roni bhakta <77425964+ronibhakta1@users.noreply.github.com>
2025-03-08 15:27:31 +0530

    fix

--- Frontend/src/components/homepage.tsx
+++ Frontend/src/components/homepage.tsx
@@ -2,10 +2,12 @@ import { Button } from "./ui/button"
 
 const Homepage = () => {
     return (
+        <>
         <div className="flex flex-col items-center justify-center min-h-svh">
             <h1 className="text-4xl font-bold">Homepage</h1>
-            <Button>Click Me</Button>
+            <Button onClick={() => console.log("clicked")}>Click Me</Button>
         </div>
+        </>
     )
 }
 
