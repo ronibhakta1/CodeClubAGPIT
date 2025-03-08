1594696b1a12340d8ef5be3cc3084967bdae3cd7
aryanmaske052005 <142209240+aryanmaske052005@users.noreply.github.com>
2025-03-08 15:33:27 +0530

    Created a file for members page

---
+++ Frontend/src/components/members/members.tsx
@@ -0,0 +1,12 @@
+
+
+const Members = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-svh">
+            <h1 className="text-4xl font-bold">Homepage</h1>
+          
+        </div>
+    )
+}
+
+export default Members
