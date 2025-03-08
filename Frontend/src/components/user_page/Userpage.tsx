4299c0126676c650ed841f8b2f9848521dc7fcfb
Nasir Nadaf <109416738+VisibleNasir@users.noreply.github.com>
2025-03-08 16:20:39 +0530

    created a Userpage file

---
+++ Frontend/src/components/user_page/Userpage.tsx
@@ -0,0 +1,10 @@
+
+const Userpage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-svh">
+            <h1 className="text-4xl font-bold">User Page</h1>   
+        </div>
+    )
+}
+
+export default Userpage
