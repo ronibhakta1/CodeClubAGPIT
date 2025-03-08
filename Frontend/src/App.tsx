785cb0766cd97c6c0967edf2f923a6ccf53115d0
Roni bhakta <77425964+ronibhakta1@users.noreply.github.com>
2025-03-08 15:53:17 +0530

    routes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -11,6 +11,9 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Homepage />} />
+      <Route path="/about" element={<h1>About</h1>} />
+      <Route path="/events" element={<h1>Events</h1>} />
+      <Route path="/contact" element={<h1>Contact</h1>} />
     </Routes>
     </BrowserRouter>
     </>
