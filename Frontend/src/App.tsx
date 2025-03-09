be2ebba0fda9bf6b88abec96c56841c819316fa3
Nasir Nadaf <109416738+VisibleNasir@users.noreply.github.com>
2025-03-10 01:04:36 +0530

    updated user page with avatar , description and edit profile button

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import Homepage from './components/homepage'
+import Userpage from './components/user_page/Userpage'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/about" element={<h1>About</h1>} />
       <Route path="/events" element={<h1>Events</h1>} />
       <Route path="/contact" element={<h1>Contact</h1>} />
+      <Route path="/userpage" element={<Userpage />} />
     </Routes>
     </BrowserRouter>
     </>
