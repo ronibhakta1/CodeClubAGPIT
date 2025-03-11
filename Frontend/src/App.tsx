1a77f702bf3c72b072dad4ad94cba0dc27a35d09
Nasir Nadaf <109416738+VisibleNasir@users.noreply.github.com>
2025-03-11 16:35:01 +0530

    updated user page with all information

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
 import Homepage from './components/homepage'
 import Members from './components/members/members'
+import Userpage from './components/user_page/Userpage'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Route path="/members" element={<Members />} />
       <Route path="/about" element={<h1>About</h1>} />
       <Route path="/events" element={<h1>Events</h1>} />
+      <Route path="/userpage" element={<Userpage />} />
       <Route path="/contact" element={<h1>Contact</h1>} />
     </Routes>
     </BrowserRouter>
---
+++ Frontend/src/components/user_page/app-sidebar.tsx
@@ -0,0 +1,50 @@
+import * as React from "react"
+import { NavProjects } from "@/components/user_page/nav-projects"
+import { NavUser } from "@/components/user_page/nav-user"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+// This is sample data.
+const data = {
+  user: {
+    name: "Roni Bhakta",
+    email: "roni@email.com",
+    avatar: "https://i.pinimg.com/736x/09/21/fc/0921fc87aa989330b8d403014bf4f340.jpg",
+  },
+  
+  projects: [
+    {
+      name: "Basic Info",
+      url: "#",
+    },
+    {
+      name: "Club Info",
+      url: "#",
+    },
+    {
+      name: "Past Events",
+      url: "#",
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavProjects projects={data.projects} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
