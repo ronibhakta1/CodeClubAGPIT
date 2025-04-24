c68574b785f248423d7c794aaf44b60cfd273d6f
VisibleNasir <nasir7777786@gmail.com>
2025-04-24 15:36:22 +0530

    Updated Colors and logos and edit options

--- Frontend/src/components/user_page/nav-main.tsx
+++ Frontend/src/components/user_page/nav-main.tsx
@@ -29,31 +29,34 @@ export function NavMain({
   setActiveSection: (key: string) => void
 }) {
   return (
+    
     <SidebarGroup>
       <SidebarGroupLabel></SidebarGroupLabel>
-      <SidebarMenu>
+      <SidebarMenu >
         {items.map((item) => (
           <Collapsible
             key={item.title}
             asChild
             defaultOpen={item.isActive}
-            className="group/collapsible"
+            className=" bg-neutral-950 text-slate-100   "
           >
             <SidebarMenuItem>
               <CollapsibleTrigger asChild>
                 <SidebarMenuButton
                   tooltip={item.title}
                   onClick={() => setActiveSection(item.sectionKey)}
-                  className={activeSection === item.sectionKey ? "bg-muted" : ""}
+                  className={ activeSection === item.sectionKey ? "text-slate-100 bg-neutral-800" : "" }
+                  
                 >
                   {item.icon && <item.icon />}
-                  <span>{item.title}</span>
+                  <span >{item.title}</span>
                 </SidebarMenuButton>
               </CollapsibleTrigger>
             </SidebarMenuItem>
           </Collapsible>
         ))}
       </SidebarMenu>
+      
     </SidebarGroup>
   )
 }
