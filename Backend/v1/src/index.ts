a3f0f746eb6d89de57526d3f240dd314effc9968
aryanmaske052005 <142209240+aryanmaske052005@users.noreply.github.com>
2025-05-19 23:30:24 +0530

    created a schema for storing data at database

--- Backend/v1/src/index.ts
+++ Backend/v1/src/index.ts
@@ -6,6 +6,43 @@ app.get("/", (c) => {
   return c.html(
     '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
   );
-});
+})
+app.get("/", (c) => {
+  return c.html(
+    '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
+  );
+})
+app.get("/staff", (c) => {
+  return c.html(
+    '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
+  );
+})
+app.get("/visitor", (c) => {
+  return c.html(
+    '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
+  );
+})
+app.get("/event", (c) => {
+  return c.html(
+    '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
+  );
+}) 
+
+app.get("/evenregistration", (c) => {
+  return c.html(
+    '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
+  );
+})
+app.get("/payment", (c) => {
+  return c.html(
+    '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
+  );
+})
+app.get("/users", (c) => {
+  return c.html(
+    '<h1 style="text-align: center ; font-size: 50px">Code Club AGPIT</h1> <h2 style="text-align: center ; font-size: 30px">Backend API</h2> <p style="text-align: center ; font-size: 20px"> <a href="https://codeclubagpit.vercel.app/"> website link </p>'
+  );
+})
+;
 
 export default app;
